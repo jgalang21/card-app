077e8f9c848fc7ea66306ff8fca3b768c73805d5
Jeremy <97.jeremy@gmail.com>
2025-01-07 19:42:42 -0600

    added a constant folder for future use with the csv

---
+++ src/components/constants/Constants.js
@@ -0,0 +1,109 @@
+// this is for the ebay fields, the logic will need to built out to use these and populate the CSV
+
+export const firstRow = ["Info", "Version=1.0.0"]
+
+export const LOT = {
+    
+    EbayID: 261329,
+    LotType: "Sports Trading Card Lots"
+
+},
+
+export const SPORTS = {
+    Basketball: "Basketball",
+    Football: "Football",
+    Baseball: "Baseball",
+    Soccer: "Soccer"
+};
+
+export const LEAGUES  = {
+    NBA: "National Basketball Association (NBA)",
+    NFL: "National Football League (NFL)",
+    MLB: "Major League Baseball"
+};
+
+
+export const secondRow = 
+
+[
+"*Action(SiteID=US|Country=US|Currency=USD|Version=1193|CC=UTF-8)",	
+"CustomLabel",	
+"*Category",	
+"StoreCategory",	
+"*Title",	
+"Subtitle",	
+"Relationship",	
+"*ConditionID",	
+"*C:Graded",	
+"*C:Sport",
+	"*C:Player/Athlete",	
+    "*C:Parallel/Variety",	
+    "*C:Manufacturer",	
+    "C:Season",	
+    "*C:Features",	
+    "*C:Set",	
+    "CD:Grade - (ID: 27502)",	
+    "*C:League",	
+    "CD:Professional Grader - (ID: 27501)",	
+    "*C:Team",	
+    "*C:Autographed",
+    "CD:Card Condition - (ID: 40001)",	
+    "*C:Card Name",	"*C:Card Number",
+    	"CDA:Certification Number - (ID: 27503)",
+        	"*C:Type",	"C:Signed By",	
+            "C:Autograph Authentication",	
+            "C:Year Manufactured",	
+            "C:Card Size",	
+            "C:Country/Region of Manufacturer",	
+            "C:Material",	
+            "C:Autograph Format",	
+            "C:Vintage",	
+            "C:Original/Licensed Reprint",	
+            "C:Event/Tournament",	
+            "C:Language",	
+            "C:Autograph Authentication Number",	
+            "C:Bundle Description",	
+            "C:California Prop 65 Warning",	
+            "C:Card Thickness",	
+            "C:Custom Bundle",	
+            "C:Insert Set",	
+            "C:Print Run",	
+            "PicURL",
+            	"GalleryType",	
+                "*Description",	
+                "*Format",	
+                "*Duration",	
+                "*StartPrice",	
+                "BuyItNowPrice",
+                	"*Quantity",	
+                    "PayPalAccepted",	
+                    "PayPalEmailAddress",
+                    	"ImmediatePayRequired",	
+                        "PaymentInstructions",	
+                        "*Location",	
+                        "PostalCode",	
+                        "ShippingType",	
+                        "ShippingService-1:Option",	
+                        "ShippingService-1:FreeShipping",	
+                        "ShippingService-1:Cost",	
+                        "ShippingService-1:AdditionalCost",
+                        	"ShippingService-2:Option",	
+                            "ShippingService-2:Cost",	
+                            "*DispatchTimeMax",	
+                            "PromotionalShippingDiscount",
+                            	"ShippingDiscountProfileID",
+                                	"*ReturnsAcceptedOption",
+                                    	"ReturnsWithinOption",
+                                        	"RefundOption",
+                                            	
+                                            "ShippingCostPaidByOption",
+                                            	"AdditionalDetails",	
+                                                "ShippingProfileName",	
+                                                "ReturnProfileName",
+                                                	"PaymentProfileName",
+                                                    	"TakeBackPolicyID",	"ProductCompliancePolicyID",	
+                                                        "ScheduleTime",	"BestOfferEnabled",
+                                                        	"MinimumBestOfferPrice",	"BestOfferAutoAcceptPrice"
+
+
+]
